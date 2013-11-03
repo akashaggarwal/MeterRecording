7aae861de8be07a253f1a74daea8b4465e3e9811
Akash Aggarwal <mac2013@akashs-imac.pk5001z>
2013-11-03 09:16:53 -0500

    Initial Commit

---
+++ MeterRecording/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MeterRecording
+//
+//  Created by Akash Aggarwal on 11/3/13.
+//  Copyright (c) 2013 Akash Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
